454e95cbe6cf1155884e9d5de756c55c71a60cf6
Adrian Kuca <adriankuca2@gmail.com>
2021-04-01 00:09:38 +0200

    Main file, short and nice.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from base64 import b64decode
 from gongon import GonGon
+import logging
+logging.basicConfig(level=logging.INFO)
 
 TOKEN = ""
 with open("./secret", "r") as f:
     TOKEN = str(b64decode(f.read()), encoding="utf-8")
 
-client = GonGon()
-client.run(TOKEN)
+GonGon.run(TOKEN)
