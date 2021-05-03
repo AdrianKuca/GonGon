63e6850f96642447b35b588066094f85f1e6f8d6
Adrian Kuca <adriankuca2@gmail.com>
2021-05-03 02:22:49 +0200

    Debug message and wake up messages.

--- gongon.py
+++ gongon.py
@@ -26,18 +26,16 @@ intents.presences = True
 GonGon = commands.Bot(command_prefix='/', intents=intents)
 
 
-
 @GonGon.event
 async def on_ready():
     '''Say hello when waking up.'''
-    #await helloChannel.send(fs.getHello() + " Wstałem.")
+    await fs.messageCreator(GonGon, fs.getHello() + " Wstałem.")
     mainGuild = dc.utils.get(GonGon.guilds, id=GUILD_ID)
     welcomeChannel = dc.utils.get(mainGuild.channels, id=WELCOME_ID)
     gonciarzTimeChannel = dc.utils.get(mainGuild.channels, id=COUNTER_CHANNEL_ID)
     gonciarzUser = dc.utils.get(mainGuild.members, id=GONCIARZ_ID)
 
     lastStatus = "offline"
-    await fs.updateGonciarzTime(gonciarzTimeChannel)
     # Main loop every second.
     ctr = 0
     while True:
@@ -59,6 +57,10 @@ async def on_ready():
             await asyncio.sleep(1)
 
         except Exception as ex:
+            fs.messageCreator(GonGon, fs.getGoodbye() + " Wyjebałem się, pomuż.\n" + str(ex))
+            print("There's been a catastrophy:", str(ex))
+            await asyncio.sleep(600)
+            
 @GonGon.command(
     name="czy", 
     help='Zadaj pytanie a przeznaczenie odpowie! Przykład: /czy kuca jest głupi'
@@ -66,7 +68,7 @@ async def on_ready():
 async def eightBall(ctx, *args):
     answer = fs.getAnswer()
     await ctx.reply(answer)
-            
+
 @GonGon.command(
     name="bombel"
 )
@@ -98,19 +100,3 @@ async def on_member_remove(member):
 
     memberCount = len([m for m in mainGuild.members if not m.bot])
     await welcomeChannel.send(fs.getGoodbye(member.mention, memberCount))
-
-@GonGon.command(
-    name="czy", 
-    help='Zadaj pytanie a przeznaczenie odpowie! Przykład: /czy kuca jest głupi'
-)
-async def eightBall(ctx, *args):
-    answer = fs.getAnswer()
-    await ctx.reply(answer)
-
-@GonGon.command(
-    name="bombel"
-)
-async def bubble(ctx, *args):
-    sideSize = 9
-    message = (("||pop||"*sideSize)+"\n")*sideSize
-    await ctx.send(message)
