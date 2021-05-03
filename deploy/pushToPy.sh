1173328abdea672910f1fdc431afa5db27365908
Adrian Kuca <adriankuca2@gmail.com>
2021-05-03 02:57:21 +0200

    Making scripts executable in unix.

--- deploy/pushToPy.sh
+++ deploy/pushToPy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 set -x
-ssh pi@192.168.0.111 "cd GonGon; git pull; sudo systemctl restart gongon"
+ssh pi@192.168.0.111 "cd GonGon; git pull; chmod a+rwx ./deploy/*; sudo systemctl restart gongon"
