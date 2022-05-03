03b293993bef75cb4301ac02f8e53b9eec6ca753
Adrian Kuca <adriankuca2@gmail.com>
2022-05-03 17:49:54 +0200

    Format gongon and fix channel ID

--- gongon.py
+++ gongon.py
@@ -11,44 +11,52 @@ import phonebook
 DEV = getenv("DEBUG", 1) == 1
 
 
-class GonGon(utilities.Utilities, nighsthift.NightShiftUtilities, timeloop.TimeLoop, commands.Bot):
+class GonGon(
+    utilities.Utilities, nighsthift.NightShiftUtilities, timeloop.TimeLoop, commands.Bot
+):
     def __init__(self) -> None:
         intents = dc.Intents.default()
         intents.members = True
         intents.presences = True
-        super().__init__(command_prefix='/', intents=intents, case_insensitive=True)
+        super().__init__(command_prefix="/", intents=intents, case_insensitive=True)
 
         # region REGISTER COMMANDS
         self.command(
             name="czy",
-            help='Zadaj pytanie a przeznaczenie odpowie! Przykład: /czy kuca jest głupi'
+            help="Zadaj pytanie a przeznaczenie odpowie! Przykład: /czy kuca jest głupi",
         )(self.eightBall)
-        self.command(
-            name="bombel"
-        )(self.bubble)
+        self.command(name="bombel")(self.bubble)
         # endregion REGISTER COMMANDS
 
     def initializeState(self):
         # region IDS
         self.mainGuild = dc.utils.get(self.guilds, id=phonebook.GUILD_ID)
         self.welcomeChannel = dc.utils.get(
-            self.mainGuild.channels, id=phonebook.WELCOME_ID)
-        self.nightChannels = [dc.utils.get(self.mainGuild.channels, id=x)
-                              for x in phonebook.NIGHT_CHANNELS_ID]
+            self.mainGuild.channels, id=phonebook.WELCOME_ID
+        )
+        self.nightChannels = [
+            dc.utils.get(self.mainGuild.channels, id=x)
+            for x in phonebook.NIGHT_CHANNELS_ID
+        ]
         self.movieChannel = dc.utils.get(
-            self.mainGuild.channels, id=phonebook.MOVIE_CHANNEL)
+            self.mainGuild.channels, id=phonebook.MOVIE_CHANNEL
+        )
         self.nightCategory = dc.utils.get(
-            self.mainGuild.categories, id=phonebook.NIGHT_CATEGORY_ID)
+            self.mainGuild.categories, id=phonebook.NIGHT_CATEGORY_ID
+        )
         self.genericRole = dc.utils.get(
-            self.mainGuild.roles, id=phonebook.GENERIC_ROLE_ID)
-        self.nightRole = dc.utils.get(
-            self.mainGuild.roles, id=phonebook.NIGHT_ROLE_ID)
+            self.mainGuild.roles, id=phonebook.GENERIC_ROLE_ID
+        )
+        self.nightRole = dc.utils.get(self.mainGuild.roles, id=phonebook.NIGHT_ROLE_ID)
         self.gonciarzTimeChannel = dc.utils.get(
-            self.mainGuild.channels, id=phonebook.COUNTER_CHANNEL_ID)
+            self.mainGuild.channels, id=phonebook.COUNTER_CHANNEL_ID
+        )
         self.mayTimeChannel = dc.utils.get(
-            self.mainGuild.channels, id=phonebook.COUNTER_CHANNEL2_ID)
+            self.mainGuild.channels, id=phonebook.COUNTER_CHANNEL2_ID
+        )
         self.gonciarzUser = dc.utils.get(
-            self.mainGuild.members, id=phonebook.GONCIARZ_ID)
+            self.mainGuild.members, id=phonebook.GONCIARZ_ID
+        )
         # endregion IDS
         # region STATE
         self.lastFunMessage = -1
@@ -58,24 +66,31 @@ class GonGon(utilities.Utilities, nighsthift.NightShiftUtilities, timeloop.TimeL
         self.nightTimeEndMinute = 00
         # endregion STATE
         # region TIMELOOP EVENTS
-        self.registerCyclicEvent(60*6, self.updateGonciarzTime)
-        self.registerCyclicEvent(60*6, self.updateMayTime)
+        # self.registerCyclicEvent(60*6, self.updateGonciarzTime)
+        self.registerCyclicEvent(60 * 6, self.updateMayTime)
         self.registerOnTimeEvent(
-            self.nightTimeStartHour, self.nightTimeStartMinute, self.openNightShift)
+            self.nightTimeStartHour, self.nightTimeStartMinute, self.openNightShift
+        )
         self.registerOnTimeEvent(
-            self.nightTimeStartHour, self.nightTimeStartMinute, self.announceNightTimeBegin)
-        self.registerOnTimeEvent(
-            2, 00, self.announceNightTimeMiddle)
-        self.registerOnTimeEvent(
-            5, 50, self.announceNightTimeEnd)
+            self.nightTimeStartHour,
+            self.nightTimeStartMinute,
+            self.announceNightTimeBegin,
+        )
+        self.registerOnTimeEvent(2, 00, self.announceNightTimeMiddle)
+        self.registerOnTimeEvent(5, 50, self.announceNightTimeEnd)
         self.registerOnTimeEvent(6, 00, self.closeNightShift)
         # endregion TIMELOOP EVENTS
+
     # region EVENTS
 
     async def on_ready(self):
-        '''Say hello when waking up and bring everything to proper states.'''
+        """Say hello when waking up and bring everything to proper states."""
         self.initializeState()
-        await self.messageCreator(self.getHello() + " Wstałem." + (" ale debugowo" if DEV else " ale produkcyjnie"))
+        await self.messageCreator(
+            self.getHello()
+            + " Wstałem."
+            + (" ale debugowo" if DEV else " ale produkcyjnie")
+        )
 
         # If starts at night, open nightshift quietly.
         now = datetime.fromtimestamp(time())
@@ -92,17 +107,21 @@ class GonGon(utilities.Utilities, nighsthift.NightShiftUtilities, timeloop.TimeL
         await self.timeLoop()
 
     async def on_message(self, message):
-        '''Respond to gonciarz when he writes on the chat.'''
-        if message.guild == self.mainGuild \
-                and message.author.id == phonebook.GONCIARZ_ID and time() - self.lastFunMessage > 60*24:
+        """Respond to gonciarz when he writes on the chat."""
+        if (
+            message.guild == self.mainGuild
+            and message.author.id == phonebook.GONCIARZ_ID
+            and time() - self.lastFunMessage > 60 * 60 * 24
+        ):
             await message.channel.send(self.getResponseToGonciarz())
             self.lastFunMessage = time()
         await super().on_message(message)
 
     async def on_member_join(self, member):
-        '''Welcome people when they join the server.'''
+        """Welcome people when they join the server."""
         memberCount = len([m for m in self.mainGuild.members if not m.bot])
         await self.welcomeChannel.send(self.getWelcome(member.mention, memberCount))
+
     # endregion EVENTS
 
     # region COMMANDS
@@ -112,6 +131,7 @@ class GonGon(utilities.Utilities, nighsthift.NightShiftUtilities, timeloop.TimeL
 
     async def bubble(self, ctx, *args):
         sideSize = 9
-        message = (("||pop||"*sideSize)+"\n")*sideSize
+        message = (("||pop||" * sideSize) + "\n") * sideSize
         await ctx.send(message)
+
     # endregion COMMANDS
